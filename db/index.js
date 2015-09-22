d2a145424ea844d5e264d8881e1941d8ef73ad87
Dnld <steinert.donald@gmail.com>
2015-09-22 09:03:33 -0400

    updates sequelize for deployment

--- db/index.js
+++ db/index.js
@@ -1,6 +1,13 @@
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize('predictster', 'root', '');
+// database for local development
+// var sequelize = new Sequelize('predictster', 'root', '');
+
+// database for deployment
+var sequelize = new Sequelize('heroku_798062d2c0ae3e4', 
+  'b7577fb0d7b6e0:9d91143f', '', {
+  host: 'us-cdbr-iron-east-02.cleardb.net',
+});
 
 exports.Predictions = Predictions = sequelize.define('Predictions', {
   entity: Sequelize.STRING,
